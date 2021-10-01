eaea72b4a6fc6384262c6a987acbb12fbecfd645
karn-karn <71032527+karn-karn@users.noreply.github.com>
2021-10-01 15:38:52 +0300

    Модальное окно

--- source/js/script.js
+++ source/js/script.js
@@ -15,3 +15,27 @@ var navLogo = document.querySelector('.page-header__logo');
       navLogo.classList.remove('page-header__logo--nav-opened');
     }
   });
+
+const formSent = document.querySelector(".form__button");
+const formPopup = document.querySelector(".modal-success");
+const formClose = formPopup.querySelector(".modal-success__button");
+
+
+formSent.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  formPopup.classList.add("js-modal-show");
+});
+
+formClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  formPopup.classList.remove("js-modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (formPopup.classList.contains("js-modal-show")) {
+      evt.preventDefault();
+      formPopup.classList.remove("js-modal-show");
+    }
+  }
+});
